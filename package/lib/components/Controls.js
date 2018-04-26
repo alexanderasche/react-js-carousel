f09d968ee7e1d9616d8113df1c464e63f764e5e6
alexanderasche <alexander.asche@gmail.com>
2018-04-26 13:58:10 -0400

    rm package dir

--- package/lib/components/Controls.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Controls = ({ events }) => {
-  const { goToPrev, goToNext } = events;
-  return [React.createElement("button", { key: "prev", className: "carousel-prev", onClick: () => goToPrev() }), React.createElement("button", { key: "next", className: "carousel-next", onClick: () => goToNext() })];
-};
-
-export default Controls;
